e69643ef694a6301e4dc2805df0b6a1a6af218ec
Unity Technologies <@unity.com>
2019-10-22 00:00:00 +0200

    com.unity.platforms.macos@0.1.5-preview ## [0.1.5-preview] - 2019-10-22
    ### Changed
    * Updated `com.unity.platforms` to `0.1.5-preview`.
    * macOS build targets `HideInBuildTargetPopup` is now `false`.
    
    ## [0.1.4-preview] - 2019-09-26
    * Bug fixes
    * Add iOS platform support
    * Add desktop platforms package

--- Editor/Unity.Platforms.macOS/MacOSBuildTarget.cs
+++ Editor/Unity.Platforms.macOS/MacOSBuildTarget.cs
@@ -1,31 +1,14 @@
 using System.Diagnostics;
 using System.IO;
+using Debug = UnityEngine.Debug;
 
 namespace Unity.Platforms.MacOS
 {
     public abstract class MacOSBuildTarget : BuildTarget
     {
-        public override bool HideInBuildTargetPopup => UnityEngine.Application.platform != UnityEngine.RuntimePlatform.OSXEditor;
-
-        public override string GetExecutableExtension()
-        {
-            return string.Empty;
-        }
-
         public override string GetUnityPlatformName()
         {
             return nameof(UnityEditor.BuildTarget.StandaloneOSX);
-        }
-
-        public override bool Run(FileInfo buildTarget)
-        {
-            var startInfo = new ProcessStartInfo();
-            startInfo.Arguments = $"\"{buildTarget.FullName.Trim('\"')}\"";
-            startInfo.FileName = Path.GetFullPath(Path.Combine(UnityEditor.EditorApplication.applicationContentsPath, "MonoBleedingEdge", "bin", "mono"));
-            startInfo.WorkingDirectory = buildTarget.Directory.FullName;
-            startInfo.CreateNoWindow = true;
-            var process = Process.Start(startInfo);
-            return process != null;
         }
     }
 
@@ -44,6 +27,58 @@ namespace Unity.Platforms.MacOS
         {
             return "macos-dotnet";
         }
+
+        public override string GetExecutableExtension()
+        {
+            return ".exe";
+        }
+
+        public override bool Run(FileInfo buildTarget)
+        {
+            var startInfo = new ProcessStartInfo();
+            startInfo.Arguments = $"\"{buildTarget.FullName.Trim('\"')}\"";
+            startInfo.FileName = Path.GetFullPath(Path.Combine(UnityEditor.EditorApplication.applicationContentsPath, "MonoBleedingEdge", "bin", "mono"));
+            startInfo.WorkingDirectory = buildTarget.Directory.FullName;
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+
+            var process = new Process();
+            process.StartInfo = startInfo;
+            process.OutputDataReceived += (_, args) => Debug.Log(args.Data);
+            process.ErrorDataReceived += (_, args) => Debug.LogError(args.Data);
+            
+            var success = process.Start();
+            if (!success)
+                return false;
+
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
+            return true;
+        }
+
+        public override ShellProcessOutput RunTestMode(string exeName, string workingDirPath, int timeout)
+        {
+            var shellArgs = new ShellProcessArgs
+            {
+                Executable = Path.GetFullPath(Path.Combine(UnityEditor.EditorApplication.applicationContentsPath, "MonoBleedingEdge", "bin", "mono")),
+                Arguments = new [] { $"\"{workingDirPath}/{exeName}{GetExecutableExtension()}\"" },
+                WorkingDirectory = new DirectoryInfo(workingDirPath),
+                ThrowOnError = false
+            };
+
+            // samples should be killed on timeout
+            if (timeout > 0)
+            {
+                shellArgs.MaxIdleTimeInMilliseconds = timeout;
+                shellArgs.MaxIdleKillIsAnError = false;
+            }
+
+            return Shell.Run(shellArgs);
+        }
+
     }
 
     class IL2CPPMacOSBuildTarget : MacOSBuildTarget
@@ -56,6 +91,56 @@ namespace Unity.Platforms.MacOS
         public override string GetBeeTargetName()
         {
             return "macos-il2cpp";
+        }
+
+        public override string GetExecutableExtension()
+        {
+            return string.Empty;
+        }
+
+        public override bool Run(FileInfo buildTarget)
+        {
+            var startInfo = new ProcessStartInfo();
+            startInfo.FileName = buildTarget.FullName.Trim('.');
+            startInfo.WorkingDirectory = buildTarget.Directory.FullName;
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+
+            var process = new Process();
+            process.StartInfo = startInfo;
+            process.OutputDataReceived += (_, args) => Debug.Log(args.Data);
+            process.ErrorDataReceived += (_, args) => Debug.LogError(args.Data);
+            
+            var success = process.Start();
+            if (!success)
+                return false;
+
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
+            return true;
+        }
+
+        public override ShellProcessOutput RunTestMode(string exeName, string workingDirPath, int timeout)
+        {
+            var shellArgs = new ShellProcessArgs
+            {
+                Executable = $"{workingDirPath}/{exeName}{GetExecutableExtension()}",
+                Arguments = new string [] {},
+                WorkingDirectory = new DirectoryInfo(workingDirPath),
+                ThrowOnError = false
+            };
+
+            // samples should be killed on timeout
+            if (timeout > 0)
+            {
+                shellArgs.MaxIdleTimeInMilliseconds = timeout;
+                shellArgs.MaxIdleKillIsAnError = false;
+            }
+
+            return Shell.Run(shellArgs);
         }
     }
 }
